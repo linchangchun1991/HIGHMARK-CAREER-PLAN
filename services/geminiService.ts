7451044ca6cd60acfa3d2e02eabf9170c7bdac36
linchangchun1991 <136042200+linchangchun1991@users.noreply.github.com>
2025-12-17 11:21:57 +0800

    refactor: Simplify Google GenAI import
    Removes unused `Type` import from `@google/genai` to streamline the dependency and reduce potential confusion.

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,5 +1,5 @@
 
-import { GoogleGenAI, Type } from "@google/genai";
+import { GoogleGenAI } from "@google/genai";
 import { AnalysisResult, StudentProfile } from "../types";
 import { PROMPT_CORE, PROMPT_VISUALS, PROMPT_ROADMAP, REAL_CASES_DB } from "../constants";
 
